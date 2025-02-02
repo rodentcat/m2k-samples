40e3a93c1f9b47d240d9680577ab19d4923b257d
rodentcat <31519042+rodentcat@users.noreply.github.com>
2025-02-01 17:53:30 -0700

    First

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# m2k-samples
-Replaces some Music 2000 samples, needs WADtool n' junk.
+![screenie](screenie.png)
+(Screenshot will eventually be inaccurate in its sample count, but I don't need to worry about that yet!)
+
+Requires WADtool: https://github.com/juju2143/wadtool
+Helpful reading from INFU: https://infu.fyi/music-software-ps1-nerdy#modding
+
+Put ANDY.IND and ANDY.WAD from the extracted PSX version (Might work with the windows version, but I've only tested the PSX version) into the root of this git, then run release_patcher.bat. It should be in the same folder as that .bat.
+Drag and drop wadtool.exe (wherever you stored that) into the command prompt that just popped up, then hit "enter"
+Boom, ya got it. Now you can put ANDY.IND and ANDY.WAD back into the software and recompile (is that the word ?) it.
---
+++ release_patcher.bat
@@ -0,0 +1,87 @@
+set /p "wadtool=Drag and drog wadtool.exe in this window, then press enter: "
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace HELPUS/ENGLISH/101/000/000 "HELPUS\101\000"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/2AND21~1 "N\B\N\A\2 and 2 1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/2AND21~1 "N\D\N\A\2 and 2 1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/2AND21~1 "N\F\N\A\2 and 2 1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/2AND21~2 "N\B\N\A\2 and 2 1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/2AND21~2 "N\D\N\A\2 and 2 1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/2AND21~2 "N\F\N\A\2 and 2 1b.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/ACT1A "N\B\N\A\act1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/ACT1A "N\D\N\A\act1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/ACT1A "N\F\N\A\act1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/ACT1B "N\B\N\A\act1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/ACT1B "N\D\N\A\act1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/ACT1B "N\F\N\A\act1b.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/ALL1A "N\B\N\A\all1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/ALL1A "N\D\N\A\all1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/ALL1A "N\F\N\A\all1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/ANDON~1 "N\B\N\A\and on.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/ANDON~1 "N\D\N\A\and on.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/ANDON~1 "N\F\N\A\and on.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/ANDYOU~1 "N\B\N\A\and you1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/ANDYOU~1 "N\D\N\A\and you1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/ANDYOU~1 "N\F\N\A\and you1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/ARYEH1~1 "N\B\N\A\ar yeh1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/ARYEH1~1 "N\D\N\A\ar yeh1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/ARYEH1~3 "N\F\N\A\ar yeh1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/ARYEH1~1 "N\F\N\A\ar yeh1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/ARYEH1~2 "N\B\N\A\ar yeh1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/ARYEH1~2 "N\D\N\A\ar yeh1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/ARYEH1~2 "N\F\N\A\ar yeh1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BACK1A "N\B\N\A\back1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BACK1A "N\D\N\A\back1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BACK1A "N\F\N\A\back1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BADDES~1 "N\B\N\A\baddest1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BADDES~1 "N\D\N\A\baddest1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BADDES~1 "N\F\N\A\baddest1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BASS1A "N\B\N\A\bass1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BASS1A "N\D\N\A\bass1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BASS1A "N\F\N\A\bass1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BASS1B "N\B\N\A\bass1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BASS1B "N\D\N\A\bass1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BASS1B "N\F\N\A\bass1b.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BEWHAT~2 "N\B\N\A\be what1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BEWHAT~2 "N\D\N\A\be what1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BEWHAT~1 "N\F\N\A\be what1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BEWHAT~1 "N\B\N\A\be what1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BEWHAT~1 "N\D\N\A\be what1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BEWHAT~2 "N\F\N\A\be what1b.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BEAT1A "N\B\N\A\beat1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BEAT1A "N\D\N\A\beat1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BEAT1A "N\F\N\A\beat1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BOMB1A "N\B\N\A\bomb1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BOMB1A "N\D\N\A\bomb1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BOMB1A "N\F\N\A\bomb1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BREAK1A "N\B\N\A\break1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BREAK1A "N\D\N\A\break1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BREAK1A "N\F\N\A\break1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BREAK1B "N\B\N\A\break1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BREAK1B "N\D\N\A\break1b.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BREAK1B "N\F\N\A\break1b.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/BROTHE~1 "N\B\N\A\brothers1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/BROTHE~1 "N\D\N\A\brothers1a.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/BROTHE~1 "N\F\N\A\brothers1a.vag"
+
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/B/N/A/CANYOU~2 "N\B\N\A\can you dig it.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/D/N/A/CANYOU~5 "N\D\N\A\can you dig it.vag"
+%wadtool% -i ANDY.IND -w ANDY.WAD replace N/F/N/A/CANYOU~3 "N\F\N\A\can you dig it.vag"
