4ab06866fc4c7bf936dd700e4a5a317e57590d1d
rodentcat <31519042+rodentcat@users.noreply.github.com>
2025-02-10 12:05:52 -0700

    convert.bat is now portable

--- N/convert.bat
+++ N/convert.bat
@@ -1,4 +1,4 @@
 set /p "id=Enter directory + name (example: N\A\act1a): "
-psxavenc.exe -t vag -f 44100hz "input.wav" "D:\Documents\Apps\Music Production\MTV Music Generator PSX\modding\work\N\F\%id%.vag"
-psxavenc.exe -t vag -f 22050hz "input.wav" "D:\Documents\Apps\Music Production\MTV Music Generator PSX\modding\work\N\D\%id%.vag"
-psxavenc.exe -t vag -f 11025hz "input.wav" "D:\Documents\Apps\Music Production\MTV Music Generator PSX\modding\work\N\B\%id%.vag"
+psxavenc.exe -t vag -f 44100hz "input.wav" "F\%id%.vag"
+psxavenc.exe -t vag -f 22050hz "input.wav" "D\%id%.vag"
+psxavenc.exe -t vag -f 11025hz "input.wav" "B\%id%.vag"
