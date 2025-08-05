20f92e69c9d204ed8a7e929fac53ba8347ef50cb
Elizabeth Sunner <31519042+rodentcat@users.noreply.github.com>
2025-08-05 16:57:29 -0600

    347 samples (final for the time being)
    yeah...

--- release_patcher.bat
+++ release_patcher.bat
@@ -1345,9 +1345,49 @@ set /p "wadtool=Drag and drog wadtool.exe in this window, then press enter: "
 %wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/D/ZERO2~1 "N\D\N\D\zero 2.vag"
 %wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/D/ZERO2~1 "N\F\N\D\zero 2.vag"
 
-%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/D/AREYOU~4 "N\B\N\D\are you 1a.vag"
-%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/D/AREYOU~4 "N\D\N\D\are you 1a.vag"
-%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/D/AREYOU~4 "N\F\N\D\are you 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/AREYOU~4 "N\B\N\F\are you 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/AREYOU~4 "N\D\N\F\are you 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/AREYOU~4 "N\F\N\F\are you 1a.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/AREYOU~1 "N\B\N\F\are you 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/AREYOU~1 "N\D\N\F\are you 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/AREYOU~1 "N\F\N\F\are you 1b.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/AREYOU~2 "N\B\N\F\are you ready 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/AREYOU~2 "N\D\N\F\are you ready 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/AREYOU~2 "N\F\N\F\are you ready 1a.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/AREYOU~3 "N\B\N\F\are you ready 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/AREYOU~3 "N\D\N\F\are you ready 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/AREYOU~3 "N\F\N\F\are you ready 1b.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/BABYBA~1 "N\B\N\F\baby baby 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/BABYBA~1 "N\D\N\F\baby baby 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/BABYBA~1 "N\F\N\F\baby baby 1a.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/BABYBA~2 "N\B\N\F\baby baby 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/BABYBA~2 "N\D\N\F\baby baby 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/BABYBA~2 "N\F\N\F\baby baby 1b.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/BACKAG~1 "N\B\N\F\back again 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/BACKAG~1 "N\D\N\F\back again 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/BACKAG~1 "N\F\N\F\back again 1a.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/BACKAG~2 "N\B\N\F\back again 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/BACKAG~2 "N\D\N\F\back again 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/BACKAG~2 "N\F\N\F\back again 1b.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/CHEKKI~1 "N\B\N\F\chekkit 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/CHEKKI~1 "N\D\N\F\chekkit 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/CHEKKI~1 "N\F\N\F\chekkit 1a.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/CHEKKI~2 "N\B\N\F\chekkit 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/CHEKKI~2 "N\D\N\F\chekkit 1b.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/CHEKKI~2 "N\F\N\F\chekkit 1b.vag"
+
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/B/N/F/COMING~1 "N\B\N\F\coming 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/D/N/F/COMING~1 "N\D\N\F\coming 1a.vag"
+%wadtool% -i %wadname%.IND -w %wadname%.WAD replace N/F/N/F/COMING~1 "N\F\N\F\coming 1a.vag"
 
 %wadtool% -i %wadname%.IND -w %wadname%.WAD replace HELPUS/ENGLISH/101/000/000 "HELPUS\101\000"
 
